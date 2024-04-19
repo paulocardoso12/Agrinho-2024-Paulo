ee5a12600b4368e93ec56a845651ad13fcdfc3da
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2024-04-19 11:14:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-const btnScrollToTop = document.getElementById("btnscrollToTop");
 
-btnScrollToTop.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
